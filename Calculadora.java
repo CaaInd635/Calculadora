2e8021b51d1b0131a32b187f41f73f9a4122d411
Echo104 <echoquequi@unsa.edu.pe>
2023-05-07 19:32:22 -0500

    Edicion codigo

--- Calculadora.java
+++ Calculadora.java
@@ -3,13 +3,14 @@ public class Calculadora
 {
 	public static void main(String args[]) {
 		int pri,sec,total,eleccion;
-		sc Scanner = new Scanner (System.in);
+		Scanner sc = new Scanner (System.in);
 		System.out.print("Ingrese primer numero:");
 		pri=sc.nextInt();
 		System.out.print("Ingrese segundo numero:");
 		sec=sc.nextInt();
-		System.out.print("Escriba el numero segun la accion: 1.Sumar");
+		System.out.println("Escriba el numero segun la accion: 1.Sumar");
 		eleccion=sc.nextInt();
+		System.out.print("Resultado: ");
 		switch(eleccion){
 			case 1:System.out.print(add(pri,sec));
 			       break;
