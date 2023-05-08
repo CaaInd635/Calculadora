68841d08555789d4df568c5a4c819a18be35cc4a
Carlos Quenta <cquenta@unsa.edu.pe>
2023-05-07 21:07:36 -0500

    Metodo div

--- Calculadora.java
+++ Calculadora.java
@@ -1,33 +1,36 @@
 import java.util.*;
-public class Calculadora
-{
-	public static void main(String args[]) {
-		int pri,sec,total,eleccion;
-		Scanner sc = new Scanner (System.in);
-		System.out.print("Ingrese primer numero:");
-		pri=sc.nextInt();
-		System.out.print("Ingrese segundo numero:");
-		sec=sc.nextInt();
-		System.out.println("Escriba el numero segun la accion: 1.Sumar 2.Restar 3.Multiplicar");
-		eleccion=sc.nextInt();
-		System.out.print("Resultado: ");
-		switch(eleccion){
-			case 1:System.out.print(add(pri,sec));
-			       break;
-			case 2:System.out.print(sub(pri,sec));
-			       break;
-			case 3:System.out.print(mul(pri,sec));
-			       break;
-		}
-			
-	}
-	public static int add(int a,int b){
-		return a+b;
-	}
-	public static int sub(int a,int b){
-		return a-b;
-	}
-	public static int mul(int a,int b){
-		return a*b;
-	}
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, ans = 0 ;
+        String sym;
+        System.out.print("Ingrese el primer numero: ");
+        num1 = sc.nextInt();
+        System.out.print("Ingrese la operacion ( + , - , * , /): ");
+        sym = sc.next();
+        System.out.print("Ingrese el primer numero: ");
+        num2 = sc.nextInt();
+        switch(sym){
+            case "+" -> System.out.println(add(num1,num2));
+            case "-" -> System.out.println(sub(num1,num2));
+            case "*" -> System.out.println(mul(num1,num2));
+            case "/" -> System.out.println(div(num1,num2));
+        }
+    }
+    public static int add(int num1, int num2){
+        int ans = num1 + num2;
+        return ans;
+    }
+    public static int sub(int num1, int num2){
+        int ans = num1 - num2;
+        return ans;
+    }
+    public static int mul(int num1, int num2){
+        int ans = num1 * num2;
+        return ans;
+    }
+    public static int div(int num1, int num2){
+        int ans = num1 / num2;
+        return ans;
+    }
 }
